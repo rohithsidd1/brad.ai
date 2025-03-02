b44f92789b5ac50257a442310850c16462d63b9c
rohith siddanathi <rohithsiddanathi.ios@gmail.com>
2025-03-02 12:09:56 +0530

    first commit

---
+++ brad.ai/BackGround.swift
@@ -0,0 +1,18 @@
+//
+//  BackGround.swift
+//  brad.ai
+//
+//  Created by apple on 28/02/25.
+//
+
+import SwiftUI
+
+struct BackGround: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BackGround()
+}
---
+++ brad.ai/Loader.swift
@@ -0,0 +1,18 @@
+//
+//  Loader.swift
+//  brad.ai
+//
+//  Created by apple on 28/02/25.
+//
+
+import SwiftUI
+
+struct Loader: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Loader()
+}
